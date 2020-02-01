6494d9cd8caf80976c5629a07dac2c20d3724b5e
Shini <shini2410@gmail.com>
2020-02-01 18:09:31 -0300

    Dinamic damage sprites for the Player

--- Enemy.gd
+++ Enemy.gd
@@ -3,17 +3,21 @@ extends KinematicBody2D
 onready var player = get_node("../Player")
 onready var enemy = get_node("../Enemy")
 var velocity = Vector2()
-var speed = 10000
+var speed = 1000
 
 
-func _process(_delta):
+func _process(delta):
 	if player.position.x > position.x:
 		velocity.x += speed
 	if player.position.x < position.x:
 		velocity.x -= speed
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		if collision.collider.name == "Player":
+			var reflect = collision.remainder.bounce(collision.normal)
+			velocity = velocity.bounce(collision.normal)
+			move_and_collide(reflect)
 	
 func _physics_process(delta):
 	velocity = move_and_slide(velocity * delta)
-
-
-
+	
--- Player.gd
+++ Player.gd
@@ -3,13 +3,15 @@ extends KinematicBody2D
 var run_speed = 350
 var jump_speed = -1000
 var gravity = 2500
-
+var HP = 5
 var velocity = Vector2()
 
 var damage1 = preload("res://damage1.png")
 var damage2 = preload("res://damage2.png")	
 var damage3 = preload("res://damage3.png")	
 var damage4 = preload("res://damage4.png")	
+var images = Array([damage1, damage2, damage3, damage4])
+onready var icon_sprite = get_node("../Player/icon")
 
 func get_input():
 	velocity.x = 0
@@ -23,15 +25,29 @@ func get_input():
 		velocity.x += run_speed
 	if left:
 		velocity.x -= run_speed
+		
+func hpCounter(Obj, Hp):
+	if Hp == 0:
+		print("Game Over")
+	elif Obj.collider.name == "Enemy":
+		HP -= 1
+		print(HP)
+		return HP
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	get_input()
 	velocity = move_and_slide(velocity, Vector2(0, -1))
-	
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		if collision.collider.name == "Enemy":
-			print(collision.collider.name)
-			var icon_sprite = get_node("../Player/icon")
-			icon_sprite.set_texture(damage1)
+		hpCounter(collision, HP)
+		match HP:
+			1:
+				icon_sprite.set_texture(damage4)
+			2:
+				icon_sprite.set_texture(damage3)
+			3:
+				icon_sprite.set_texture(damage2)
+			4:
+				icon_sprite.set_texture(damage1)
+		
